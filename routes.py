9620b16669fb5b01af7bfc34a46ae01b1af9d4ad
Marti Curto <marti_cv@hotmail.com>
2024-04-02 10:16:42 +0200

    test gitignore

--- routes.py
+++ routes.py
@@ -1,2 +1,2 @@
 carpeta_origen = "C:\\Users\\Marti Curto Vendrell\\Python\\origen"
-carpeta_destino = "C:\\Users\\Marti Curto Vendrell\\Python\\files"
+carpeta_destino = "C:\\Users\\Marti Curto Vendrell\\Python\\files\\test"
